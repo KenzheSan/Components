302a3ae5912d20a537c40eb5c16d3eb1dad8e4b9
KenzheSan <js3nursultan@gmial.com>
2021-11-04 17:01:33 +0600

    Hard-work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Wrapper from "./Component/Wrapper/Wrapper"
-
+import './App.css'
+import Marquee from "react-fast-marquee";
 
 function App() {
 
 	
 	return (
 		<div className='main-wrapper'>
-			<Wrapper/>
+			<Marquee className='text' speed={200}  >Welcome to Main Page</Marquee>
+			<Wrapper/> 
 		</div>
 	)
 }
