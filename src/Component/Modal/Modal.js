e7ca798d426f3170e98138e9186a24c1a5648823
KenzheSan <js3nursultan@gmial.com>
2021-11-10 11:16:36 +0600

    Hard_work

--- src/Component/Modal/Modal.js
+++ src/Component/Modal/Modal.js
@@ -1,51 +1,78 @@
 import './Modal.css'
 import Button from '../Ui/Button'
+import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-const Modal = (props) => {
+
+const BlackDrop = props =>{
 	return (
 		<div
-			className={props.modal ? 'module active' : 'module'}
+		className={props.modal ? 'module active' : 'module'}
+		onClick={(e) => e.stopPropagation()}
+	>
+		<div
+			className={
+				props.modal ? 'module_content active' : 'module_content'
+			}
 			onClick={(e) => e.stopPropagation()}
 		>
-			<div
-				className={
-					props.modal ? 'module_content active' : 'module_content'
-				}
-				onClick={(e) => e.stopPropagation()}
-			>
-                	<div className='mainblog'>
-					<div className='userblog'>
-						<input
-							id='username'
-							placeholder='User-Name'
-							type='name'
-						/>
-						<br />
-						<input
-							id='password'
-							placeholder='Password'
-							type='password'
-						/>
-					</div>
+			<div className='mainblog'>
+				<div className='userblog'>
+					<input
+						id='username'
+						placeholder='User-Name'
+						type='name'
+					/>
 					<br />
-					<Button className='create' onClick={()=> {
+					<input
+						id='password'
+						placeholder='Password'
+						type='password'
+					/>
+				</div>
+				<br />
+				<Button
+					className='create'
+					onClick={() => {
 						props.setModal(false)
-					}}>Login</Button>
+					}}
+				>
+					Login
+				</Button>
 
-					<header className='headerblog'>
-						<Button className='create' onClick={()=>{
+				<header className='headerblog'>
+					<Button
+						className='create'
+						onClick={() => {
 							props.setActive(true)
 							props.setModal(false)
-						}}>Sign Up</Button>
-					</header>
-					<Button className='fixed' onClick={()=>{
+						}}
+					>
+						Sign Up
+					</Button>
+				</header>
+				<Button
+					className='fixed'
+					onClick={() => {
 						props.setActive(false)
 						props.setModal(false)
-					}}>Close All</Button>
-				</div>
-			
+					}}
+				>
+					Close All
+				</Button>
 			</div>
 		</div>
+	</div>
+	)
+}
+const Modal = (props) => {
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<BlackDrop modal={props.modal} setModal={props.setModal}  setActive={props.setActive}/>,
+				document.getElementById('blackdrop')
+			)}
+		</React.Fragment>
 	)
 }
 export default Modal
--- src/Component/ModalRe/ModalRe.js
+++ src/Component/ModalRe/ModalRe.js
@@ -1,51 +1,69 @@
 import Button from '../Ui/Button'
 import './ModalRe.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-const ModalRe = (props) => {
+const BlackDropBlog = (props) => {
 	return (
 		<div
 			className={props.modal ? 'module active' : 'module'}
 			onClick={(e) => e.stopPropagation()}
-		>
-			<div
-				className={
-					props.modal ? 'module_content active' : 'module_content'
-				}
-				onClick={(e) => e.stopPropagation()}
-			>
-				<div className='mainblog'>
-					<h1>Registration</h1>
-					<div className='userblog'>
-						<input
-							id='username'
-							placeholder='User-Name'
-							type='name'
-						/>
-						<br />
-						<input
-							id='password'
-							placeholder='password'
-							type='password'
-						/>
-						<br />
-						<input id='password' placeholder='email' type='email' />
-					</div>
-					<br />
-					<header className='headerblog'>
-						<Button className='create' onClick={() => props.setModal(false)}>Create</Button>
-					</header>
-					<Button
-						className='fixed'
-						onClick={() => {
-							props.setActive(false)
-							props.setModal(false)
-						}}
-					>
-						Close All
-					</Button>
-				</div>
-			</div>
+		>		<div
+		className={props.modal ? 'module_content active' : 'module_content'}
+		onClick={(e) => e.stopPropagation()}>
+			{props.children}
+				</div>			
 		</div>
+	)
+}
+
+const BlackDropContent = props =>{
+	return (
+		<div className='mainblog'>
+		<h1>Registration</h1>
+		<div className='userblog'>
+			<input id='username' placeholder='User-Name' type='name' />
+			<br />
+			<input
+				id='password'
+				placeholder='password'
+				type='password'
+			/>
+			<br />
+			<input id='password' placeholder='email' type='email' />
+		</div>
+		<br />
+		<header className='headerblog'>
+			<Button
+				className='create'
+				onClick={() => props.setModal(false)}
+			>
+				Create
+			</Button>
+		</header>
+		<Button
+			className='fixed'
+			onClick={() => {
+				props.setActive(false)
+				props.setModal(false)
+			}}
+		>
+			Close All
+		</Button>
+	</div>
+	)
+}
+
+const ModalRe = (props) => {
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<BlackDropBlog modal={props.modal} setModal={props.setModal}  setActive={props.setActive}>
+					<BlackDropContent modal={props.modal} setModal={props.setModal}  setActive={props.setActive}/>
+				</BlackDropBlog>,
+				document.getElementById('modalRe')
+			)}
+		</React.Fragment>
 	)
 }
 export default ModalRe
